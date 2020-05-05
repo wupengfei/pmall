25dba293814cd5a27c7c50816a9a721fd09161e5
wupengfei <wupengfei@perry.com>
2020-05-05 12:38:50 +0800

    add demo project

---
+++ demo/src/main/java/edu/bjtu/sse/DemoApplication.java
@@ -0,0 +1,14 @@
+package edu.bjtu.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
